668ab7d6e6b3d666ed0e0a98e39a3f2f384bf98b
Sayam Pradhan <112542130+sayampradhan@users.noreply.github.com>
2024-08-25 01:55:29 +0530

    Update WeatherTunes.py

--- WeatherTunes.py
+++ WeatherTunes.py
@@ -41,8 +41,8 @@ if location:
                             spotify_url = recommended_song["url"]
 
                             st.markdown(f"**Recommended Song:** {track_name}")
-                            st.markdown(f"[Listen to this song on Spotify]({spotify_url})")
-                            st.markdown('<img src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/84/Spotify_icon.svg/232px-Spotify_icon.svg.png" width=20>Play: '+spotify_url,unsafe_allow_html=True)
+                            st.markdown(f"[Listen to this song on Spotify]({spotify_url})" + '<img src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/84/Spotify_icon.svg/232px-Spotify_icon.svg.png" width=20>Play: ')
+                            # st.markdown()
 
                             # Additional Media Player Example (optional)
                             st_player(url=spotify_url)
